0fd971f55811ab7fef28aa03d609122834108946
Lanceconan <conan_022@hotmail.com>
2018-08-24 00:49:54 -0300

    Tipos de Datos y test de typescript

---
+++ tipodato.js
@@ -0,0 +1,16 @@
+"use strict";
+//texto
+var vartexto = "Hola mundo";
+//numero
+var varNumero = 123;
+//boleano
+var verdadero = true;
+var falso = false;
+//any
+var multivariable = "primero texto";
+multivariable = 1212;
+multivariable = true;
+//array
+var arrayString = ["bella", "chao"];
+var arrayNumero = [1, 2.3, 2.5];
+var arrayCualquiera = ["bella", "chao", 12, false, "miranda"];
---
+++ tipodato.ts
@@ -0,0 +1,19 @@
+//texto
+let vartexto:string = "Hola mundo";
+
+//numero
+let varNumero:number = 123;
+
+//boleano
+let verdadero:boolean = true;
+let falso:boolean = false;
+
+//any
+let multivariable:any = "primero texto";
+multivariable = 1212;
+multivariable = true;
+
+//array
+let arrayString:string[] = ["bella", "chao"];
+let arrayNumero:number[] = [1, 2.3, 2.5];
+let arrayCualquiera:any[] = ["bella", "chao", 12, false, "miranda"];
